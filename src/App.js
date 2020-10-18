e7bf23d2496714eba7edfdb01673ff965e3f1a54
Shubham Singh <shubham88singh@gmail.com>
2020-10-19 03:19:11 +0530

    adding base routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router';
 import './App.css';
+import Settings from './pages/Settings';
+import UpdateInformation from './pages/UpdateInformation';
 
 function App() {
   return (
     <div className="App">
-     
+      <Router>
+        <Switch>
+          <Route exact path="/settings">
+            <Settings />
+          </Route>
+          <Route exact path="/update">
+            <UpdateInformation />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/UpdateInformation/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UpdateInformation = () => {
+  return <div></div>;
+};
+
+export default UpdateInformation;
